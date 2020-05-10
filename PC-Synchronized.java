a1f4c3316c2f426cc3dedfbfc6965486afb850eb
gxflove307 <56139203+gxflove307@users.noreply.github.com>
2020-05-10 15:16:25 +0100

    Update PC-Synchronized.java

--- PC-Synchronized.java
+++ PC-Synchronized.java
@@ -1,109 +1,84 @@
-    package com.lzb.common;    
-        
-    import java.util.Random;    
-    import java.util.concurrent.TimeUnit;    
-    /**  
-     *   
-     * 功能描述：生产者消费者  
-     *          注：锁synhronized是放在“资源的类的内部方法中”，而不是在线程的代码中  
-     */    
-    public class ProducterCustomer {    
-            
-        private PCResource pc = new PCResource();    
-        // 生产者与消费者调用的时间随机    
-        private Random rand = new Random(50);    
-            
-        public void init() {            
-            // 生产者    
-            new Thread(new Runnable(){    
-        
-                public void run() {                 
-                    while(true) {    
-                        pc.producter();    
-                        try {    
-                            TimeUnit.MILLISECONDS.sleep(rand.nextInt(1000));    
-                        } catch (InterruptedException e) {    
-                            e.printStackTrace();    
-                        }    
-                    }    
-                        
-                }}).start();    
-            
-            // 消费者    
-            new Thread(new Runnable(){    
-        
-                public void run() {    
-                    while(true) {    
-                        pc.customer();    
-                        try {    
-                            TimeUnit.MILLISECONDS.sleep(rand.nextInt(1000));    
-                        } catch (InterruptedException e) {    
-                            e.printStackTrace();    
-                        }    
-                    }    
-                }}).start();    
-        }    
-            
-        public static void main(String[] args) {    
-                
-            ProducterCustomer startPc = new ProducterCustomer();    
-            startPc.init();    
-        }           
-    }    
-        
-    /**  
-     *   
-     * 功能描述：同步资源  
-     *  
-     */    
-    class PCResource {    
-            
-        private static final Integer MAX = 1;    
-        private static final Integer MIN = 0;    
-        private int product = 0;    
-        // 同步互斥通信标志    
-        private boolean isRunning = true;    
-            
-        /**  
-         *   
-         * 功能描述：生产产品，当生产一个商品后挂起，等待消费者消费完成  
-         */    
-        public synchronized void producter() {    
-                
-            while(isRunning) {    
-                try {    
-                    wait();    
-                } catch (InterruptedException e) {    
-                    e.printStackTrace();    
-                }    
-                product++;    
-                System.out.println("-------->Product " + product + " good");    
-                if(product >= MAX)    
-                    break;    
-            }    
-            isRunning = false;    
-            notify();    
-        }    
-            
-        /**  
-         *   
-         * 功能描述：消费者，消费产品，当产品为0时，等待生产者生产产品  
-         */    
-        public synchronized void customer() {    
-                
-            while(!isRunning) {    
-                try {    
-                    wait();    
-                } catch (InterruptedException e) {    
-                    e.printStackTrace();    
-                }    
-                product--;    
-                System.out.println("Limit " + product + " goods<----------");    
-                if(product <= MIN) {    
-                    break;    
-                }    
-            }    
-            isRunning = true;    
-            notify();    
-        }    
-    }    
+public class MyConsumerProductor {
+	ArrayList<String> list=new ArrayList<>();
+	final private int Capacity = 10;
+	//生产者
+	class Consumer implements Runnable{
+		@Override
+		public void run() {
+			while(true) {
+				synchronized(MyConsumerProductor.class) {
+					while(list.size()==Capacity) {//循环判断,因为激活多个线程
+						try {
+							System.out.println("容量满了，生产者进入等待");
+							MyConsumerProductor.class.wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+ 
+					if(list.size()<10) {
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						list.add(Thread.currentThread().getName());
+						System.out.println("product:"+Thread.currentThread().getName()+" size:"+list.size());
+						if(list.size()==10) {
+							MyConsumerProductor.class.notifyAll();
+						}
+					}
+				}
+			}
+		}
+	}
+ 
+	//生产者
+	class Productor implements Runnable{
+		@Override
+		public void run() {
+			while(true) {
+				synchronized(MyConsumerProductor.class) {
+					while(list.size()==0) {
+						try {
+							System.out.println("容量没了，消费者进入等待");
+							MyConsumerProductor.class.wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+ 
+					if(list.size()>0) {
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						System.out.println("consumer:"+list.remove(list.size()-1)+" size:"+list.size());
+						if(list.size()==Capacity) {
+							MyConsumerProductor.class.notifyAll();
+						}
+					}
+				}
+			}
+		}
+	}
+ 
+ 
+	public static void main(String[] args) {
+		MyConsumerProductor pc=new MyConsumerProductor();
+		//生产者线程
+		for(int i=0;i<5;i++) {
+			new Thread(pc.new Consumer()).start();
+		}
+		//消费者线程
+		for(int i=0;i<5;i++) {
+			new Thread(pc.new Productor()).start();
+		}
+	}
+ 
+}
